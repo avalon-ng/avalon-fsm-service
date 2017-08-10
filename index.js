7cfd8cfd66fd761ea2575da50834e5f240e9a629
jeno5980515 <jeno5980515@yahoo.com.tw>
2017-08-10 09:36:51 +0800

    add fsm-avalon

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { getFSM, actionCreators } = require('fsm-avalon');
+
 const app = express();
 const port = 4001;
+
+const { reducer, getValidateError, getActions } = getFSM();
+
+const { 
+	initGame,
+	startRound,
+	buildTeam,
+	vote,
+	drawVotesResult,
+	executeMission,
+	drawMissionsResult,
+	assassin
+} = actionCreators ;
+
 
 app.use(bodyParser.json());      
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.post('/', function (req, res) {
+app.post('/', (req, res) => {
 	const { state, payload } = req.body;
   res.status(400).json({state, payload});
 })
 
-app.listen(4001)
+app.post('/initGame', (req, res) => {
+	const { state, payload } = req.body;
+	let newState = reducer(undefined, {});
+	newState = reducer(newState, initGame(payload));
+	res.status(400).json(newState);
+})
+
+app.listen(4001);
+
